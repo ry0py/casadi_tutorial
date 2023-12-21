93609becb7caa1a1314dae90fbb2852954d339ce
ry0py <ryoppy101@gmail.com>
2023-12-21 23:45:09 +0900

    copy_gpt

--- src/trajectory_creator.py
+++ src/trajectory_creator.py
@@ -1,53 +1,47 @@
-# 必要なライブラリをインポート
-from casadi import SX, vertcat, MX, DM, Function, integrator, nlpsol, inf
+from casadi import *
 import numpy as np
-
 def main():
-    # モデルパラメータと変数の定義
-    X, Y, theta, V, beta, theta_dot, beta_dot = SX.sym('X Y theta V beta theta_dot beta_dot')
-    x = vertcat(X, Y, theta, V, beta, theta_dot, beta_dot)
-    theta_ddot, beta_ddot = SX.sym('theta_ddot beta_ddot')
-    u = vertcat(theta_ddot, beta_ddot)
-    x_dot = vertcat(V * np.cos(beta), V * np.sin(beta), theta_dot, theta_ddot, beta_ddot)
-
-    # 目的関数（時間の最小化）
-    final_time = MX.sym('final_time')
-    objective = final_time
-
+    # 状態変数の設定
+    X =SX.sym('X')
+    Y =SX.sym('Y')
+    theta =SX.sym('theta')
+    V =SX.sym('V')
+    beta =SX.sym('beta')
+    theta_dot =SX.sym('theta_dot')
+    beta_dot =SX.sym('beta_dot')
+    # 状態ベクトルに統合
+    x = vertcat(X,Y,theta,V,beta,theta_dot,beta_dot)
+    # 入力変数の設定
+    theta_ddot =SX.sym('theta_ddot')
+    beta_ddot =SX.sym('beta_ddot')
+    # 入力ベクトルに統合
+    u = vertcat(theta_ddot,beta_ddot)
+    # 微分方程式の設定
+    x_dot = vertcat(V*np.cos(beta),V*np.sin(beta),theta_dot,theta_ddot,beta_dot,beta_ddot)
+    
+    
+    # 目的関数の設定(時間を最小化)
+    cost_time = SX.sym('cost_time') # TODO SXだとエラーが出る
+    obj = cost_time
+    
     # 制約条件の設定
-    # ここでは仮の制約を設定しています。実際の問題に応じて調整してください。
-    # ...
-
-    # 始点、経過点、終点の座標と向き
-    X0, Y0, theta0 = 0, 0, 0  # 始点
-    X1, Y1, theta1 = 5, 5, np.pi/4  # 経過点
-    X2, Y2, theta2 = 10, 10, np.pi/2  # 終点
-
-    # 始点、経過点、終点の制約
-    start_point_constraint = vertcat(X - X0, Y - Y0, theta - theta0)
-    pass_point_constraint = vertcat(X - X1, Y - Y1, theta - theta1)
-    end_point_constraint = vertcat(X - X2, Y - Y2, theta - theta2)
-
-    # 制約の統合
-    all_constraints = vertcat(start_point_constraint, pass_point_constraint, end_point_constraint, ...)
-
-    # 最適化問題の定義
-    nlp = {'x': vertcat(x, u), 'f': objective, 'g': all_constraints}
-    opts = {'ipopt': {'max_iter': 1000}}
-    solver = nlpsol('solver', 'ipopt', nlp, opts)
-
-    # 初期条件、終端条件、制約条件の設定
-    lbg = [0] * len(all_constraints)  # 下限制約
-    ubg = [0] * len(all_constraints)  # 上限制約
-    lbx = [-inf] * len(x) + [-inf] * len(u)  # 状態と制御の下限
-    ubx = [inf] * len(x) + [inf] * len(u)  # 状態と制御の上限
-    x0 = [0] * len(x) + [0] * len(u)  # 初期値
-
-    # 問題を解く
-    sol = solver(lbg=lbg, ubg=ubg, lbx=lbx, ubx=ubx, x0=x0)
-
-    # 解を表示
-    print('Solution:', sol['x'])
-
+    steer_acc_constraint = 1
+    steer_speed_constraint = 1
+    # ユーザーが設定する
+    max_force = 100 
+    max_power = 100
+    max_cf = 100
+    
+    # 制約条件の統合
+    g = vertcat(steer_acc_constraint,steer_speed_constraint,max_force,max_power,max_cf)
+    
+    # 最適問題の設定
+    nlp = {'x':x,'f':obj,'g':g}
+    opts = {"ipopt.linear_solver":"ma27"} # ソルバーの設定
+    solver = nlpsol('solver','ipopt',nlp,opts) # 最適化問題の設定
+    
+    sol = solver (x0=[0,0,0,0,0,0,0],lbx=[-inf,-inf,-inf,-inf,-inf,-inf,-inf],ubx=[inf,inf,inf,inf,inf,inf,inf],lbg=[-inf,-inf,-inf,-inf,-inf],ubg=[inf,inf,inf,inf,inf]) # ソルバーの設定
+    
+    print('x_opt: ',sol['x'])
 if __name__ == '__main__':
     main()
